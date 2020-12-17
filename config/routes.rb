7d0ea9b6e6e785309261b39554a68e6cbdf73744
AMANBANDUNI <abanduni.5@gmail.com>
2020-12-17 18:39:59 +0530

    added route for the form path to products#selected_destroy

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
 
   get "/un-delete/:id" => "products#undeleted"
 
+  # post "/products_destroy_path", to: "products#selected_destroy"
+
+  post "selected_destroy", to: "products#selected_destroy", as: "products_destroy"
+
 end
